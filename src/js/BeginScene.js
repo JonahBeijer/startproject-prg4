fad6dabfd173ca75a74b402ada481e64ccbf647b
JonahBeijer <145674761+JonahBeijer@users.noreply.github.com>
2024-06-13 01:26:54 +0200

    BeginScene.js

--- src/js/BeginScene.js
+++ src/js/BeginScene.js
@@ -3,21 +3,11 @@ import { Scene, Label, Color, Vector, Loader, Sprite, CoordPlane, TextAlign, Fon
 import { Resources } from './resources';
 
 export class BeginScene extends Scene {
-
-    
-        
-          
-    
-
-        
-        Expand All
-    
-    @@ -12,18 +12,24 @@ export class BeginScene extends Scene {
-  
     constructor(game) {
         super();
         this.game = game;
     }
+
     onInitialize(engine) {
         const title = new Label({
             text: 'Pinkie',
@@ -43,36 +33,32 @@ export class BeginScene extends Scene {
         });
 
         startButton.on('pointerup', () => {
-
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
             this.game.goToScene('main');
         });
+
         // Maak een nieuwe Loader
         const loader = new Loader();
+
         // Voeg de bron toe aan de loader
         // Voeg de bron toe aan de loader
     loader.addResource(Resources.PixelYe); 
+
     // Laad de bronnen
     loader.load().then(() => {
         // Maak een sprite van de geladen bron
         const playerSprite = new Sprite(Resources.PixelYe);
+
         // Plaats de sprite in het midden van het scherm
         playerSprite.pos = new Vector(engine.drawWidth / 2, engine.drawHeight / 2);
+
         // Voeg de sprite toe aan de scene
         this.add(playerSprite);
     }).catch((error) => {
         console.error('Error loading resources:', error);
     });
+
         
+
         this.add(title);
         this.add(startButton);
     }
